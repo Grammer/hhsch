38b2ab471edac6de1f9bd8703d513d3de9c04cb7
Anton Leonov <grmmr07@gmail.com>
2014-10-08 15:51:05 +0400

    small fix
    fixed a bug that made a median int, not float

--- python/median.py
+++ python/median.py
@@ -7,5 +7,5 @@ for i in range(0, arr_num):
     second_arr.append(int(raw_input("Enter the numbers to second array :")))
 final_arr = sorted(first_arr + second_arr)
 print final_arr
-median = (final_arr[len(final_arr)/2 - 1] + final_arr[len(final_arr)/2])/2
+median = (final_arr[len(final_arr)/2 - 1] + final_arr[len(final_arr)/2])/2.0
 print median
